8c3243ae59394a77bd4ba54d2facac8f0f7e1113
Jason Au <kin.jasonau@gmail.com>
2019-03-12 12:56:20 -0400

    Update Diameter_PeakDetection.m

--- Diameter_PeakDetection.m
+++ Diameter_PeakDetection.m
@@ -1,24 +1,20 @@
-%Picking off peaks (Anterograde, Retrograde, and 2nd Anterograde) in MAUI Doppler velocity export
-%Click 'Run' and select the MAUI diameter export file with your data. The
-%code with read in column 6 (media-media) to pull off maximum and minimum
-%arterial diameters, and export them to a separate excel file
-
+%% Picking off peaks (Anterograde, Retrograde, and 2nd Anterograde) in MAUI Doppler velocity export
 
 close all;
 clear all;
 
-%Open the individual file
+%Select the individual file
 [filename,pathname] = uigetfile('*.*');
 path = char(pathname);
 [row,files_selected] = size(filename);
 filename = cellstr(filename);
 
-    
 tempfile = char(filename);
 disp(tempfile);
 
 data = xlsread(tempfile);
 
+%Column information
 %1 = frame number
 %5 = time
 %6 = media-media I2-I7 (cm)
@@ -29,23 +25,21 @@ data = xlsread(tempfile);
 frame = data(:,1);
 time = data(:,5);
 I27 = data(:,6);
-% nIMT = data(:,7);
+% nIMT = data(:,7); %IMT isn't identifiable in brachial artery images, so ignore for now.
 % fIMT = data(:,8);
 % I45 = data(:,10);
 
 len = length(I27);
 timevec = 1:1:len;
 
-%Plot trace to screen data and check peaks
+%Plot trace to screen data and check peaks. Identify how many cycles you want to analyze
 fig0 = figure('Name','Initial check');
 plot(time,I27);
-
 numPeaks = str2num(cell2mat(inputdlg('How many cycles are you analyzing?')));
 
 %Find Systolic peaks of I4-I5
 fig1 = figure('Name','Systolic Peaks');
 set(fig1, 'Position', [50 300 450 300]);
-
 [Syspks,Syspeaklocs] = findpeaks(I27,'MinPeakDistance',20,'NPeaks',numPeaks);
 plot(timevec,I27,timevec(Syspeaklocs),Syspks,'or');
 hold;
@@ -55,7 +49,6 @@ ylabel('Diameter (cm)');
 
 %Find retrograde peaks
 peakDias = I27*-1;          %Flip signal to get the minimums
-
 fig2 = figure('Name','Diastolic Peaks');
 xlabel('Time (sec)');
 ylabel('Diameter (cm)');
@@ -72,13 +65,10 @@ sysmean = mean(outmat(:,1));
 diasmean = mean(outmat(:,2));
 meanmean = mean(I27);
 
-out(1,1) = sysmean;
-out(1,2) = diasmean;
-out(1,3) = meanmean;
+out(1,1) = sysmean; %Average of systolic peaks
+out(1,2) = diasmean; %Average of diastolic minimums
+out(1,3) = meanmean; %Mean diameter for entire image
 
-%%
-out(1,1) = mean(realsys(:,2));
-out(1,2) = -mean(realdias(:,2));
-out(1,3) = mean(meand(:,2));
 
+%Export if you want
 % xlswrite('Diameters_temp.xlsx',outmat);
